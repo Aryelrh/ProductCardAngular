183a6f96d1ee7fb40d9291650031e068c8b10f59
Fernanda Rojas <maria.rojas.j@est.una.ac.cr>
2026-05-04 18:13:10 -0600

    Add Card and CardList components; switch to Vite
    Introduce Card and CardList components with templates, styles, and unit tests. Card exposes a product input and increments quantity via handleAddCart; CardList uses Angular signals and a computed total to manage a sample product list. Update package.json to add a "dev" script and use Vite for build, and set Angular CLI analytics to false in angular.json.

---
+++ src/app/components/card-list/card-list.ts
@@ -0,0 +1,45 @@
+import { Component, signal, effect, computed } from '@angular/core';
+import { Card } from '../card/card';
+
+@Component({
+  selector: 'app-card-list',
+  imports: [Card],
+  templateUrl: './card-list.html',
+})
+export class CardList {
+  cards = signal([
+    {
+      id: 1,
+      name: 'Card 1',
+      description: 'Description for Card 1',
+      price: 19.99,
+      qt: 0,
+      imageUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_vyrSDRWsdKQwxQDh2o4H7fxrrHR0Qntyng&s',
+    },
+    {
+      id: 2,
+      name: 'Card 2',
+      description: 'Description for Card 2',
+      price: 29.99,
+      qt: 0,
+      imageUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_vyrSDRWsdKQwxQDh2o4H7fxrrHR0Qntyng&s',
+    },
+    {
+      id: 3,
+      name: 'Card 3',
+      description: 'Description for Card 3',
+      price: 39.99,
+      qt: 0,
+      imageUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_vyrSDRWsdKQwxQDh2o4H7fxrrHR0Qntyng&s',
+    },
+  ]);
+
+  total = computed(() => {
+    return this.cards().reduce((a, b) => a + b.price * b.qt, 0);
+  });
+
+  constructor() {}
+}
---
+++ src/app/components/card/card.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  imports: [],
+  templateUrl: './card.html',
+})
+export class Card {
+  @Input() product: {
+    name: string;
+    description: string;
+    price: number;
+    imageUrl?: string;
+    qt: number;
+    id: number;
+  } | null = null;
+
+  @Input() addToCart?: (id: number) => void;
+
+  constructor() {}
+
+  handleAddCart() {
+    if (this.product) {
+      this.product.qt = this.product.qt + 1;
+    }
+  }
+}
